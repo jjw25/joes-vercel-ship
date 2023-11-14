2649b3bf639e374ea53bca2f88831954658bb970
Joe W <106349199+jjw25@users.noreply.github.com>
2023-11-14 15:37:03 -0500

    Update MyV0Component.tsx

--- app/components/MyV0Component.tsx
+++ app/components/MyV0Component.tsx
@@ -1,62 +1,54 @@
-// you can overwrite this entire file with your v0 Component.
-// just copy and paste the "React" output over the entire file.
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/6KBhKBw
+ */
+import { CardContent, CardTitle, CardDescription, CardHeader, Card } from "@/components/ui/card"
 
-import Link from "next/link";
-import React from "react";
-import {V0Logo} from "./symbols";
-
-function TestComponent() {
+export default function Component() {
   return (
-    <div
-      style={{
-        display: "flex",
-        height: "100%",
-        justifyContent: "space-between",
-        flexDirection: "column",
-        width: "100%",
-      }}
-    >
-      <h3
-        style={{
-          maxWidth: "11em",
-        }}
-      >
-        This would be a pretty good place for a{" "}
-        <Link href="https://v0.dev/" target="_blank" rel="noopener noreferrer">
-          v0 component
-        </Link>
-        , wouldn't it?
-      </h3>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "flex-end",
-          width: "100%",
-        }}
-      >
-        <p
-          style={{
-            maxWidth: "20em",
-          }}
-        >
-          Go make one, then paste it into{" "}
-          <code
-            style={{
-              fontFamily: "var(--font-geist-mono)",
-              fontWeight: 500,
-              fontSize: "0.95em",
-              fontFeatureSettings: "'ss09'",
-            }}
-          >
-            app/components/MyV0Component.tsx
-          </code>
-        </p>
-
-        <V0Logo />
+    <div className="flex items-center justify-center min-h-screen bg-cyan-500">
+      <div className="max-w-lg mx-auto bg-cyan-500 p-6 rounded-lg shadow-offset-black offset-x-10 offset-y-10">
+        <Card className="p-6 font-poppins">
+          <CardContent className="text-xl relative mb-2">
+            <svg
+              className=" absolute top-0 left-0 text-20xl text-cyan-300"
+              fill="none"
+              height="24"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              width="24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M3 21c3 0 7-1 7-8V5c0-1.25-.756-2.017-2-2H4c-1.25 0-2 .75-2 1.972V11c0 1.25.75 2 2 2 1 0 1 0 1 1v1c0 1-1 2-2 2s-1 .008-1 1.031V20c0 1 0 1 1 1z" />
+              <path d="M15 21c3 0 7-1 7-8V5c0-1.25-.757-2.017-2-2h-4c-1.25 0-2 .75-2 1.972V11c0 1.25.75 2 2 2h.75c0 2.25.25 4-2.75 4v3c0 1 0 1 1 1z" />
+            </svg>
+            "Generating a testimonial card component with a prompt-driven interface is pretty cool... Well done,
+            Vercel!"
+          </CardContent>
+          <CardHeader className="pt-0">
+            <div className="flex items-center">
+              <img
+                alt="John Doe's Avatar"
+                className="mr-2 rounded-full"
+                height="40"
+                src="/placeholder.svg"
+                style={{
+                  aspectRatio: "40/40",
+                  objectFit: "cover",
+                }}
+                width="40"
+              />
+              <div>
+                <CardTitle className="text-base">John Doe</CardTitle>
+                <CardDescription>CEO, Example Corp.</CardDescription>
+              </div>
+            </div>
+          </CardHeader>
+        </Card>
       </div>
     </div>
-  );
+  )
 }
-
-export default TestComponent;
